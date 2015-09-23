00eb7c40d10156c384e56045c7ba0887ecc7fff9
Rikysamuel <rikz.samuel@gmail.com>
2015-09-23 21:22:02 +0700

    core weka j48

--- src/j48/MyJ48.java
+++ src/j48/MyJ48.java
@@ -2,6 +2,7 @@ package j48;
 
 import weka.classifiers.Classifier;
 import weka.classifiers.Sourcable;
+import weka.classifiers.trees.j48.*;
 import weka.core.*;
 
 import java.util.Enumeration;
@@ -9,7 +10,42 @@ import java.util.Enumeration;
 /**
  * Created by rikysamuel on 9/22/2015.
  */
-public class MyJ48 extends Classifier implements OptionHandler, Drawable, Matchable, Sourcable, WeightedInstancesHandler, Summarizable, AdditionalMeasureProducer, TechnicalInformationHandler {
+public class MyJ48 extends Classifier implements OptionHandler, Drawable, Matchable, Sourcable,
+        WeightedInstancesHandler, Summarizable, AdditionalMeasureProducer, TechnicalInformationHandler {
+
+    private ClassifierTree root;
+    private boolean bUnpruned = false;
+    private float fCF = 0.25f;
+    private int iMinInstances = 2;
+    private boolean bEnableLaplace = false;
+    private boolean bReducedErrorPruning = false;
+    private int iReducedErrorPruningNumFolds = 3;
+    private boolean bBinarySplitsNominalAttribute = false;
+    private boolean bSubtreeRaising = true;
+    private boolean bNoCleanup = false;
+    private int seedReducedErrorPruning = 1;
+
+
+    @Override
+    public Capabilities getCapabilities() {
+        Capabilities      result;
+
+        try {
+            if (!bReducedErrorPruning)
+                result = new MyC45PruneableClassifierTree(null, !bUnpruned, fCF, bSubtreeRaising,
+                        !bNoCleanup).getCapabilities();
+            else
+                result = new PruneableClassifierTree(null, !bUnpruned, iReducedErrorPruningNumFolds, !bNoCleanup,
+                        seedReducedErrorPruning).getCapabilities();
+        }
+        catch (Exception e) {
+            result = new Capabilities(this);
+        }
+
+        result.setOwner(this);
+
+        return result;
+    }
 
     @Override
     public Enumeration enumerateMeasures() {
@@ -23,27 +59,61 @@ public class MyJ48 extends Classifier implements OptionHandler, Drawable, Matcha
 
     @Override
     public void buildClassifier(Instances instances) throws Exception {
+        ModelSelection modSelection;
 
+        if (bBinarySplitsNominalAttribute)
+            modSelection = new BinC45ModelSelection(iMinInstances, instances);
+        else
+            modSelection = new C45ModelSelection(iMinInstances, instances);
+        if (!bReducedErrorPruning)
+            root = new C45PruneableClassifierTree(modSelection, !bUnpruned, fCF,
+                    bSubtreeRaising, !bNoCleanup);
+        else
+            root = new PruneableClassifierTree(modSelection, !bUnpruned, iReducedErrorPruningNumFolds,
+                    !bNoCleanup, seedReducedErrorPruning);
+        root.buildClassifier(instances);
+        if (bBinarySplitsNominalAttribute) {
+            ((BinC45ModelSelection)modSelection).cleanup();
+        } else {
+            ((C45ModelSelection)modSelection).cleanup();
+        }
+    }
+
+    @Override
+    public double classifyInstance(Instance instance) throws Exception {
+        return root.classifyInstance(instance);
+    }
+
+    @Override
+    public double[] distributionForInstance(Instance instance) throws Exception {
+        return root.distributionForInstance(instance, bEnableLaplace);
     }
 
     @Override
     public int graphType() {
-        return 0;
+        return Drawable.TREE;
     }
 
     @Override
     public String graph() throws Exception {
-        return null;
+        return root.graph();
     }
 
     @Override
     public String prefix() throws Exception {
-        return null;
+        return root.prefix();
     }
 
     @Override
-    public String toSource(String s) throws Exception {
-        return null;
+    public String toSource(String classAttribute) throws Exception {
+        StringBuffer [] source = root.toSource(classAttribute);
+        return "class ".concat(classAttribute).concat( " {\n\n")
+                        .concat("  public static double classify(Object[] i)\n")
+                        .concat("    throws Exception {\n\n")
+                        .concat("    double p = Double.NaN;\n")+ source[0]  // Assignment code
+                        + "    return p;\n"
+                        .concat("  }\n") + source[1]  // Support code
+                        + "}\n";
     }
 
     @Override
@@ -53,6 +123,38 @@ public class MyJ48 extends Classifier implements OptionHandler, Drawable, Matcha
 
     @Override
     public TechnicalInformation getTechnicalInformation() {
-        return null;
+        TechnicalInformation result = new TechnicalInformation(TechnicalInformation.Type.ARTICLE);
+        result.setValue(TechnicalInformation.Field.AUTHOR, "Cilvia Sianora Putri, Steve Immanuel Harnadi, and Rikysamuel");
+        result.setValue(TechnicalInformation.Field.YEAR, "2015");
+        result.setValue(TechnicalInformation.Field.TITLE, "MyID3 implementation");
+
+        return result;
+    }
+
+    public int getSeed() {
+       return seedReducedErrorPruning;
+    }
+
+    public void setSeedReducedErrorPruning(int seedReducedErrorPruning) {
+        this.seedReducedErrorPruning = seedReducedErrorPruning;
+    }
+
+    public boolean isbEnableLaplace() {
+        return bEnableLaplace;
+    }
+
+    public void setbEnableLaplace(boolean bEnableLaplace) {
+        this.bEnableLaplace = bEnableLaplace;
+    }
+
+    public String toString() {
+
+        if (root == null) {
+            return "No classifier built";
+        }
+        if (bUnpruned)
+            return "J48 unpruned tree\n------------------\n" + root.toString();
+        else
+            return "J48 pruned tree\n------------------\n" + root.toString();
     }
 }
